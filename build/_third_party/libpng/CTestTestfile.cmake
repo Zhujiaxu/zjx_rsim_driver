2d43a9325d791b204cc33f6cd9e9f464f5394e81
ubzjx <3462922427@qq.com>
2026-06-04 10:12:03 +0800

    gitversion_03

--- build/_third_party/libpng/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubzjx/Proj/Intern_QCen/work/rsim-driver/third_party/libpng
-# Build directory: /home/ubzjx/Proj/Intern_QCen/work/rsim-driver/build/_third_party/libpng
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
